6fe34e7502ca325318dcb580754008da23c90b7a
EudesN <eudesnazario178@gmail.com>
2025-03-19 03:22:03 +0000

    Mar 19, 2025, 12:22 AM

--- SomaMatrizes.py
+++ SomaMatrizes.py
@@ -2,5 +2,11 @@ A = [[1, 2], [3, 4]]
 
 B = [[5, 6], [7, 8]]
 
-for i in len(A):
-    C
+for i in A:
+    print(A)
+
+def somarMatrizes(A, B):
+    X = []
+    for i in  range(len(A)):
+        linha
+    return 
