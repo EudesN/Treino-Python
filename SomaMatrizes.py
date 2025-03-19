ae9b3dcb97c657a92c92be4a334ffa9f978cbea3
EudesN <eudesnazario178@gmail.com>
2025-03-19 03:29:27 +0000

    Mar 19, 2025, 12:29 AM

--- SomaMatrizes.py
+++ SomaMatrizes.py
@@ -2,11 +2,32 @@ A = [[1, 2], [3, 4]]
 
 B = [[5, 6], [7, 8]]
 
-for i in A:
-    print(A)
-
 def somarMatrizes(A, B):
     X = []
     for i in  range(len(A)):
-        linha
-    return 
+        linha = []
+        for j in range(len(A[0])):
+            linha.append(A[i][j] + B[i][j])
+        X.append(linha)
+    return X
+
+def subtraiMatrizes(A, B):
+    Y = []
+    for i in range(len(A)):
+        linha = []
+        for j in range(len(A[0])):
+            linha.append(A[i][j] - B[i][j])
+        Y.append(linha)
+    return Y
+
+C = somarMatrizes(A, B)
+D = subtraiMatrizes(A, B)
+
+for linha in C:
+    print(linha)
+
+print("-"*20)
+
+for linha in D:
+    print(linha)
+
