a1e1caea66fae4b5778d53e505e381d217e6781f
EudesN <eudesnazario178@gmail.com>
2025-03-20 01:21:22 +0000

    Mar 19, 2025, 10:21 PM

--- Tuplas.py
+++ Tuplas.py
@@ -1,8 +1,29 @@
 
 def calculoArea(terreno):
-    base, al
+    base, alt = terreno
+    return base * alt
 
 terreno = []
 
-base = float(input("Informe a base: "));
-alt = float()
+for i in range(3):
+    print(f"Informações do terreno {i + 1}")
+    base = float(input("Informe a base: "))
+    alt = float(input("Informe o valor da altura: "))
+    terreno.append((base, alt))
+
+print("Terrenos: ")
+for i, terreno in enumerate(terreno, 1):
+    print(f"Terreno {i}: Base = {terreno[0]}m, Altura = {terreno[1]}m")
+
+areas = [calculoArea(terreno) for terra in terreno]
+
+maior = max(areas)
+menor = min(areas)
+
+print(f"\nA maior área é: {maior} m²")
+print(f"A menor área é: {menor} m²")
+
+
+
+
+
