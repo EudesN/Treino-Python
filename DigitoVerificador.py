1390d08a4d62b3b6bf2156c54da8fa265cc4bc52
EudesN <eudesnazario178@gmail.com>
2025-03-22 23:21:20 +0000

    Mar 22, 2025, 8:21 PM

--- DigitoVerificador.py
+++ DigitoVerificador.py
@@ -4,5 +4,25 @@ def calculoDV(cpf):
         return "O CPF precisa possuir 9 digitos"
     
     peso1 = [10, 9, 8, 7, 6, 5, 4, 3, 2]
-    for i in range(9):
-        sum(int(cpf[i]) * peso1[i])
+    soma1 = sum(int(cpf[i]) * peso1[i] for i in range(9))
+    res = soma1 % 11
+
+    if res < 2:
+        dv1 = 0
+    else:
+        dv1 = 11 - res
+    
+    peso2 = [11, 10, 9, 8, 7, 6, 5, 4, 3, 2]
+    soma2 = sum(int(cpf[i]) * peso2[i] for i in range(9)) + dv1 * 2
+    res2 = soma2 % 11
+
+    if res2 < 2:
+        dv2 = 0
+    else:
+        dv2= 11 - res2
+
+    print(f"{cpf[:3]} {cpf[3:6]} - {dv1} {dv2}")
+
+cpf = input("Digite os 9 primeiros dígitos do CPF (sem pontos ou hífen): ")
+calculoDV(cpf)
+
