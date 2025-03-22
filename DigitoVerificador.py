5a3d4107f94f4516b06ef3cecd9824498eddd67e
EudesN <eudesnazario178@gmail.com>
2025-03-22 22:57:31 +0000

    Mar 22, 2025, 7:57 PM

--- DigitoVerificador.py
+++ DigitoVerificador.py
@@ -1,1 +1,7 @@
-cpf = int(input("Informe o cpf"))
+
+def calculoDV(cpf):
+    if len(cpf) != 9 or not cpf.isdigit(): 
+        return "O CPF precisa possuir 9 digitos"
+    
+    peso1 = [10, 9, 8, 7, 6, 5, 4, 3, 2]
+    
