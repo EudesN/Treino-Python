881abbfe53793308587fc12686472516b4ba02a5
EudesN <eudesnazario178@gmail.com>
2025-03-29 11:54:05 +0000

    Mar 29, 2025, 8:54 AM

--- ArrayRedimencional.py
+++ ArrayRedimencional.py
@@ -19,8 +19,43 @@ def inserirFinal(self, elemento):
 def inserirInicio(self, elemento):
     if self.quantVal == self.tam:
         self.redimenciona(2* self.tam)
-        # desloca a lista uma posição para direita para caber um valor na pos 0
-        for i in range(self.quantVal, 0, -1): 
+        for i in range(self.quantVal, 0, -1): # desloca a lista uma posição para direita para caber um valor na pos 0
             self.dados[i] = self.dados[i - 1]
         self.dados[0] = elemento
         self.quantVal += 1
+        print(f"{elemento} foi adicionado ao inicio com sucesso")
+
+def exluir(self, elemento):
+    for i in range(self.quantVal):
+        if self.dados == elemento:
+
+            for j in range(i, self.quantVal - 1):  # Desloca os elementos para a esquerda
+                self.dados[j] = self.dados[j + 1]
+
+            self.dados[self.tamanho - 1] = None
+            self.quantVal -= 1
+            print(f"{elemento} foi removido com sucesso")
+            return
+        print(f"{elemento} não está na lista")
+
+def buscar(self, elemento):
+    for i in range(self.quantVal):
+        if self.dados[i] == elemento:
+            print(f"O elemento {elemento} foi encotrado na posição {i}")
+    print(f"{elemento} não foi encontrado na lista")
+
+
+def redimenciona(self, novoTam):
+    novaLista = [None] * novoTam
+    for i in range(self.quantVal): # Copia os elementos para a nova lista
+        novoTam[i] = self.dados[i]
+
+    self.dados = novaLista # troca a lsta antiga pela nova
+    self.tam = novoTam # atualiza a capacidade da lista
+
+
+
+
+
+
+
