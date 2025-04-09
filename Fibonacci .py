80fa26bba729c913992e7222d8050989d699263e
EudesN <eudesnazario178@gmail.com>
2025-04-09 03:35:48 +0000

    Apr 9, 2025, 12:35 AM

--- Fibonacci .py
+++ Fibonacci .py
@@ -1,3 +1,8 @@
-Fibonacci = 0
-
-n = int(input("informe um número: "))
+n1 = 0
+n2 = 1
+num = int(input("informe um número: "))
+while(n1 <= num):
+    print(n1, end= " ")
+    prox = n1 + n2
+    n1 = n2 
+    n2 = prox 
