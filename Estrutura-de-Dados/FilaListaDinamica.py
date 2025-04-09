78c1af59e183f38af06561b604f32d343d24902b
EudesN <eudesnazario178@gmail.com>
2025-04-09 22:22:58 +0000

    Apr 9, 2025, 7:22 PM

--- Estrutura-de-Dados/FilaListaDinamica.py
+++ Estrutura-de-Dados/FilaListaDinamica.py
@@ -8,8 +8,9 @@ class Fila:
 
     def enqueue(self, nome):
         if self.tam == self.cap: # antes de inserir verifica se o array está cheio 
-            self._redimencionar(self.cap * 2) # se sim, é necessario chamar a função _redimencionar
-        self.dados[self.tam] = nome # Insere o nome na próxima posição livre (índice = tamanho atual)
+            self._redimencionar(self.cap * 2) # se sim, é necessario chamar a função redimencionar
+        self.dados[self.fim] = nome
+        self.fim = (self.fim + 1) % self.cap
         self.tam += 1
 
     def dequeue(self):
