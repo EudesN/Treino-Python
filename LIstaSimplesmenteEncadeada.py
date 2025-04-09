0241e673d4b1bc50221a3f050228fc121dbf4b1c
EudesN <eudesnazario178@gmail.com>
2025-04-09 03:50:27 +0000

    Apr 9, 2025, 12:50 AM

--- LIstaSimplesmenteEncadeada.py
+++
@@ -1,8 +0,0 @@
-class No:
-    def __init__(self, dado):
-        self.dado = dado        # O valor armazenado no nó
-        self.proximo = None     # Referência para o próximo nó
-
-class ListaEncadeada:
-    def __init__(self):
-        self.pElemento  = None
