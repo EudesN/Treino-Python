97d4f12fd777bd6f86cf193ff8596f6f01287737
EudesN <eudesnazario178@gmail.com>
2025-04-09 21:37:12 +0000

    Apr 9, 2025, 6:37 PM

--- Estrutura-de-Dados/FilaListaDinamica.py
+++ Estrutura-de-Dados/FilaListaDinamica.py
@@ -9,7 +9,7 @@ class Fila:
     def enqueue(self, nome):
         if self.tam == self.cap:
             self._redimencionar(self.cap * 2)
-        self.dados[self.fim] = None
+        self.dados[self.fim] = nome
         self.fim = (self.fim + 1) % self.cap
         self.tam += 1
 
@@ -24,10 +24,53 @@ class Fila:
 
         #reduz a capacidade
         if 0 < self.tam <= self.cap // 4 and self.cap > 4:
-            self._redimencionar(self.cap / 2)
+            self._redimencionar(self.cap // 2)
+        return nome
     
     def _redimencionar(self, novaCap):
         novoArray = [None] * novaCap
-    
+
+        for i in range(self.tam):
+            novoArray[i] = self.dados[(self.inicio + i) % self.cap]
+        self.dados = novoArray
+        self.inicio = 0
+        self.fim = self.tam
+        self.cap = novaCap
+
     def empty(self):
-        return len(self) == 0
+        return self.tam == 0
+    
+    def show(self):
+        print("\nFila atual: ")
+        for i in range(self.tam):
+             print(f"- {self.dados[(self.inicio + i) % self.cap]}")
+        print(f"Total de pessoas: {self.tam}")
+
+fila = Fila()
+
+while True:
+    print("\n=== MENU ===")
+    print("1. Enfileirar pessoa")
+    print("2. Desenfileirar pessoa")
+    print("3. Mostrar fila")
+    print("0. Sair")
+    opcao = input("Escolha uma opção: ")
+
+    if opcao == '1':
+        nome = input("Digite o nome da pessoa: ")
+        fila.enqueue(nome)
+        print(f"{nome} entrou na fila.")
+
+    elif opcao == '2':
+        nome = fila.dequeue()
+        if nome:
+            print(f"{nome} foi removido da fila.")
+
+    elif opcao == '3':
+        fila.show()
+
+    elif opcao == '0':
+        print("Encerrando o programa.")
+        break
+    else:
+        print("Opção inválida. Tente novamente.")
