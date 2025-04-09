cb410c98071666c013695d5b86fe152ebdda99f6
EudesN <eudesnazario178@gmail.com>
2025-04-09 02:35:52 +0000

    Apr 8, 2025, 11:35 PM

--- Lista1-Lab.Progamacao/NumPares.py
+++ Lista1-Lab.Progamacao/NumPares.py
@@ -1,6 +1,6 @@
 pares = []
 n = int(input("Informe um número: "))
-for num in range(n):
-    if(n % 2 == 0):
+for num in range(n + 1):
+    if(num % 2 == 0):
         pares.append(num)
 print(pares)
