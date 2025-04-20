a0747e550156fd981173b9c9bf2e24ea4ceb5ed9
EudesN <eudesnazario178@gmail.com>
2025-04-20 11:46:51 +0000

    Apr 20, 2025, 8:46 AM

--- Lista2-Lab.Programacao/Conjutos.py
+++ Lista2-Lab.Programacao/Conjutos.py
@@ -1,15 +1,25 @@
-conjunto1 = {"vermelho", "azul", "verde"}
-conjunto2 = {"amarelo", "azul", "laranja"}
-conjunto3 = {"azul", "verde", "preto"}
+cores1 = {"vermelho", "azul", "verde"}
+cores2 = {"amarelo", "azul", "laranja"}
+cores3 = {"azul", "verde", "preto"}
 
 def selConjunto():
-    while True:
+    try:
         print("\nEscolha dois entre os conjuntos 1,2 e 3: ")
         a = int(input("Selecione o primeiro conjunto: "))
         b = int(input("Selecione o segundo conjunto: "))
+        if(a not in [1, 2, 3] or b not in [1, 2, 3]):
+            print("Conjunto inválido")
+            return None, None
+        conjuntos = {1: cores1, 2: cores2, 3: cores3}
+        return conjuntos[a], conjuntos[b]
+    
+    except ValueError:
+        print("seleção inválida")
+        return None, None
 
 
 while True:
+
     print("\nMenu de opções:")
     print("1 - União entre dois conjuntos")
     print("2 - Interseção entre dois conjuntos")
@@ -17,6 +27,12 @@ while True:
     print("4 - Sair do programa")
 
     opcao = input("Informe a opção: ")
+
+    if opcao == '1':
+        conj1, conj2 = selConjunto()
+        if conj1 is not None and conj2 is not None:
+            resultado = c1.union(c2)
+            print("Resultado da união:", resultado)
 
 
 
