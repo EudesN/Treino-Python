966d817945659bb4cd5ba271379129d9d3ee6dd5
EudesN <eudesnazario178@gmail.com>
2025-04-20 11:28:29 +0000

    Apr 20, 2025, 8:28 AM

---
+++ Lista2-Lab.Programacao/Conjutos.py
@@ -0,0 +1,18 @@
+conjunto1 = {"vermelho", "azul", "verde"}
+conjunto2 = {"amarelo", "azul", "laranja"}
+conjunto3 = {"azul", "verde", "preto"}
+
+
+
+while True:
+    print("\nMenu de opções:")
+    print("1 - União entre dois conjuntos")
+    print("2 - Interseção entre dois conjuntos")
+    print("3 - Diferença entre dois conjuntos")
+    print("4 - Sair do programa")
+
+    opcao = input("Informe a opção: ")
+
+
+
+
