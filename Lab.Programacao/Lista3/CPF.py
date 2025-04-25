cf45e85684118d2948554a7ff061b4a6f69a752c
EudesN <eudesnazario178@gmail.com>
2025-04-25 12:52:36 +0000

    Apr 25, 2025, 9:52 AM

--- Lab.Programacao/Lista3/CPF.py
+++ Lab.Programacao/Lista3/CPF.py
@@ -5,14 +5,38 @@ def inserirPessoa(dados):
 
     nome = str(input("Informe seu nome: "))
     endereco = input("Informe seu endereço: ")
-    Telefone = input("Informe seu telefone: ")
+    telefone = input("Informe seu telefone: ")
 
     dados[cpf] = {'Nome': nome, 'Endereço': endereco, 'Telefone': telefone }
 
+    print("pessoas cadastrada com sucesso")
 
+# def listarPessoas(dados):
 
+def menu():
+    pessoas = {}
 
+while True:
+    
 
+    print("1. Inserir pessoa")
+    print("2. Listar pessoas cadastradas")
+    print(" 3. Buscar pessoa por CPF")
+    print("4. Buscar pessoa por Telefone")
+    print(" 5. Remover pessoa por CPF")
+    print(" 6. SAIR")
 
+    opcao = input("Informe a opção desejada: ")
 
+    if opcao == '1':
+        inserirPessoa(pessoas)
+    
 
+    if opcao == '6':
+        print("Encerrando programa...")
+        break
+
+    else:
+        print("Opcão Inválida")
+
+menu()
