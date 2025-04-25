9c951946a1bf60643c794f1d668b06959df02d16
EudesN <eudesnazario178@gmail.com>
2025-04-25 12:39:40 +0000

    Apr 25, 2025, 9:39 AM

--- Lab.Programacao/Lista3/CPF.py
+++ Lab.Programacao/Lista3/CPF.py
@@ -1,4 +1,13 @@
-dados = {'CPF': None, 'Nome': None, 'Endereço': None, 'Telefone': []  }
+def inserirPessoa(dados):
+    cpf = int(input("Informe o cpf: "))
+    if cpf in dados:
+        return
+
+    nome = str(input("Informe seu nome: "))
+    endereco = input("Informe seu endereço: ")
+    Telefone = input("Informe seu telefone: ")
+
+    dados[cpf] = {'Nome': nome, 'Endereço': endereco, 'Telefone': telefone }
 
 
 
