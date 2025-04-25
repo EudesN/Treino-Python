1fa624e68fad7df5d94d6f5b1502af99f5b4d213
EudesN <eudesnazario178@gmail.com>
2025-04-25 23:28:06 +0000

    Apr 25, 2025, 8:28 PM

--- Lab.Programacao/Lista3/CPF.py
+++ Lab.Programacao/Lista3/CPF.py
@@ -7,7 +7,7 @@ def inserirPessoa(dados):
     endereco = input("Informe seu endereço: ")
     telefone = input("Informe seu telefone: ").split(",")
 
-    dados[cpf] = {'Nome': nome, 'Endereço': endereco, 'Telefones': telefone }
+    dados[cpf] = {'Nome': nome, 'Endereco': endereco, 'Telefones': telefone }
 
     print("pessoas cadastrada com sucesso")
 
@@ -17,9 +17,9 @@ def listarPessoas(dados):
         return
     for cpf, info in dados.items():
         print(f"CPF: {cpf}")
-        print(f"Nome: {dados['Nome']}")
-        print(f"Endereço: {dados['Endereco']}")
-        print(f"Telefones: {', '.join(dados['Telefones'])}")
+        print(f"Nome: {info['Nome']}")
+        print(f"Endereço: {info['Endereco']}")
+        print(f"Telefones: {', '.join(info['Telefones'])}")
         print("-" * 30)
 
 def buscarPorCpf(dados):
@@ -40,7 +40,7 @@ def buscarPorCpf(dados):
 def buscarPorTelefone(dados):
     telBusca = input("Informe o telefone para a busca: ")
     for cpf, pessoa in dados.items():
-        if telBusca in pessoa['Telefone']:
+        if telBusca in pessoa['Telefones']:
             print(f"Nome: {pessoa['Nome']}")
             print(f"Endereço: {pessoa['Endereco']}")
             print(f"Telefones: {', '.join(pessoa['Telefones'])}")
@@ -58,37 +58,39 @@ def removerPorCpf(dados):
 def menu():
     pessoas = {}
 
-while True:
-    
+    while True:
+        
+        print("-" * 30)
+        print("1. Inserir pessoa")
+        print("2. Listar pessoas cadastradas")
+        print("3. Buscar pessoa por CPF")
+        print("4. Buscar pessoa por Telefone")
+        print("5. Remover pessoa por CPF")
+        print("6. SAIR")
+        print("-" * 30)
 
-    print("1. Inserir pessoa")
-    print("2. Listar pessoas cadastradas")
-    print(" 3. Buscar pessoa por CPF")
-    print("4. Buscar pessoa por Telefone")
-    print(" 5. Remover pessoa por CPF")
-    print(" 6. SAIR")
+        opcao = input("Informe a opção desejada: ")
 
-    opcao = input("Informe a opção desejada: ")
+        if opcao == '1':
+            inserirPessoa(pessoas)
+        
+        elif opcao == '2':
+            listarPessoas(pessoas)
 
-    if opcao == '1':
-        inserirPessoa(pessoas)
-    
-    if opcao == '2':
-        listarPessoas(pessoas)
+        elif opcao == '3':
+            buscarPorCpf(pessoas)
 
-    if opcao == '3':
-        buscarPorCpf(pessoas)
+        elif opcao == '4':
+            buscarPorTelefone(pessoas)
+        
+        elif opcao == '5':
+            removerPorCpf(pessoas)
+        
+        elif opcao == '6':
+            print("Encerrando programa...")
+            break
 
-    if opcao == '4':
-        buscarPorTelefone(pessoas)
-    
-    if opcao == '5':
-        removerPorCpf(pessoas)
-    if opcao == '6':
-        print("Encerrando programa...")
-        break
-
-    else:
-        print("Opcão Inválida")
+        else:
+            print("Opcão Inválida")
 
 menu()
