ee242a923c478c87773941cf0b6ceb8c31135df4
EudesN <eudesnazario178@gmail.com>
2025-04-28 14:23:44 +0000

    Apr 28, 2025, 11:23 AM

--- Elementary Sorts/bubble sort.py
+++ Elementary Sorts/bubble sort.py
@@ -13,13 +13,14 @@ lista_repetidos = [5, 5, 3, 3, 3, 8, 8, 1, 1, 1, 1, 7, 7, 7, 2, 2, 9, 9, 9, 9]
 
 
 
-lista_ordenada = bubble_sort(lista_aleatoria)
-lista_ordenada = bubble_sort(lista_invertida)
-lista_ordenada = bubble_sort(lista_repetidos)
+bubble_sort(lista_aleatoria)
+bubble_sort(lista_invertida)
+bubble_sort(lista_repetidos)
 
-print(f"Lista aleatória ordenada: {lista_ordenada}")
+print(f"Lista aleatória ordenada: {lista_aleatoria}")
 print(f"Lista invertida ordenada: {lista_invertida}")
 print(f"Lista repetida ordenada: {lista_repetidos}")
+
 
 
 
