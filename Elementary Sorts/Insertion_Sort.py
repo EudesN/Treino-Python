9ea1676d88e3b3ff8ed51cbe7d5d0b4cf009f8b0
EudesN <eudesnazario178@gmail.com>
2025-04-29 01:51:24 +0000

    Apr 28, 2025, 10:51 PM

---
+++ Elementary Sorts/Insertion_Sort.py
@@ -0,0 +1,12 @@
+def insertion_sort(lista):
+    n = len(lista)
+    for i in range(1, n - 1): # O primeiro elemento é ignirádo pois uma lista com um unico elemento já está ordenada
+        chave = lista[i]
+
+
+
+
+
+
+
+
