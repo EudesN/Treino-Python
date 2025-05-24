ea1ab705262060918ddd5db36d7634637b4a1c67
EudesN <eudesnazario178@gmail.com>
2025-05-24 19:11:10 +0000

    May 24, 2025, 4:11 PM

--- Elementary Sorts/Selection sort.py
+++ Elementary Sorts/Selection sort.py
@@ -1,10 +1,11 @@
 def selection_sort(lista):
     min_index = 0
     for j in range(len(lista) - 1): # controla a lista que precisa ser analizada a cada interação. E '-1' pq o ultimo elemento já vai estar ordenado ao final
-        for i in range(len(lista)):
+        min_index = j # o inidice minimo é inciado em j pq o 
+        for i in range(j, len(lista)):
             if lista[i] < lista[min_index]: # encontrar o menor valor
-                min_index = j # armazena o menor valor, prcurando dentro dos valores q ainda não estão ordenados
-        if lista[j] > lista[min_index]: # realiza a troca do menor valor para a prossição indicada por j
+                min_index = i # armazena o menor valor, prcurando dentro dos valores q ainda não estão ordenados
+        if lista[j] > lista[min_index]: # realiza a troca do menor valor para a prosição indicada por j
             aux = lista[j]
             lista[j] = lista[min_index]
             lista[min_index] = aux
