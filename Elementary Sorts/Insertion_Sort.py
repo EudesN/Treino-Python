516d276a1c9de21545c614fbff578f195a66f4f2
EudesN <eudesnazario178@gmail.com>
2025-05-24 20:25:11 +0000

    May 24, 2025, 5:25 PM

--- Elementary Sorts/Insertion_Sort.py
+++ Elementary Sorts/Insertion_Sort.py
@@ -2,9 +2,27 @@ def insertion_sort(lista):
     n = len(lista)
     for i in range(1, n - 1): # O primeiro elemento é ignirádo pois uma lista com um unico elemento já está ordenada
         chave = lista[i]
-        j = i - 1
-        while j >=0 and lista[j]
+        j = i - 1 # representa a parcela da lista que já está ordenada
+        while j >= 0 and lista[j] > chave:
+            lista[j + 1] = lista[j]
+            j -= 1
+        lista[j + 1] = chave
 
+
+lista_aleatoria = [12, 5, 23, 8, 3, 17, 14, 29, 6, 2, 18, 9, 11, 30, 7, 21, 1, 4, 25, 15]
+lista_invertida = [28, 22, 19, 16, 13, 12, 11, 9, 7, 5, 4, 3, 2, 2, 1, 1, 0, 0, -2, -5]
+lista_repetidos = [5, 5, 3, 3, 3, 8, 8, 1, 1, 1, 1, 7, 7, 7, 2, 2, 9, 9, 9, 9]
+
+insertion_sort(lista_aleatoria)
+insertion_sort(lista_invertida)
+insertion_sort(lista_repetidos)
+
+
+
+
+print(f"Lista aleatória ordenada: {lista_aleatoria}")
+print(f"Lista invertida ordenada: {lista_invertida}")
+print(f"Lista repetida ordenada: {lista_repetidos}")
 
 
 
