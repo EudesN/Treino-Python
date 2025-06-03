d857ad3e75d095e778b7d064d0e4ce7f66d87999
EudesN <eudesnazario178@gmail.com>
2025-06-03 04:47:00 +0000

    Jun 3, 2025, 1:47 AM

--- Estrutura-de-Dados/Elementary Sorts/HashTable.py
+++ Estrutura-de-Dados/Elementary Sorts/HashTable.py
@@ -14,9 +14,9 @@ class HashTable:
         return hash(chave) % self.M # Usa a função interna do Python para converter a chave em um número inteiro grande
     
     def get(self,chave):
-        pos = self._hash(chave)
+        pos = self._hash(chave) # qual índice procurar
         aux = self.tab[pos]
-        while aux:
+        while aux: # enquanto existir um Node na lista encadeada
             if aux.key == chave:
                 return aux.value
             aux = aux.prox
@@ -26,19 +26,19 @@ class HashTable:
         pos = self._hash(chave) # calcula o indice 
         if self.tab[pos] is None: # se a posição for vazia cria um novo nó com chave e valor
             self.tab[pos] = Node(chave, valor)
-        else: # caso haja colisão 
+        else: # caso haja colisão : já existe pelo menos um Node ocupando esse índice
             aux = self.tab[pos]
             while aux: # percorre a lista de nós que já existe naquela posição
                 if aux.key == chave:
-                    aux.value = valor
+                    aux.value = valor # Caso a chave já exista, simplesmente atualiza o valor
                     return
                 aux = aux.prox
-            
+            # Caso não encontre a chave após percorrer toda lista, insiro no início
             novo = Node(chave, valor)
             novo.prox = self.tab[pos]
             self.tab[pos] = novo
         
-        self.N += 1
+        self.N += 1 # atualiza o contador total de pares inseridos
 
 
 
