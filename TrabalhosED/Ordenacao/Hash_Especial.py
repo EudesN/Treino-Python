9f642a6e3503971c46112dc72214178bb3dcdd6b
EudesN <eudesnazario178@gmail.com>
2025-06-10 11:15:35 +0000

    Jun 10, 2025, 8:15 AM

--- TrabalhosED/Ordenacao/Hash_Especial.py
+++ TrabalhosED/Ordenacao/Hash_Especial.py
@@ -4,7 +4,14 @@ class Node:
         self.value = valor
         self.next = None
 
+class HashTable:
+    def __init__(self, cap):
+        self.M = cap
+        self.N = 0
+        self.tab = cap * [None]
 
+def _hash(self, chave):
+    return hash(chave) % self.M
 
 
 
