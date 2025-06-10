dc28ae4cda5283636c8ea9a55a065cb32014392d
EudesN <eudesnazario178@gmail.com>
2025-06-10 11:26:27 +0000

    Jun 10, 2025, 8:26 AM

--- TrabalhosED/Trabalho_Individual/Hash_Especial.py
+++ TrabalhosED/Trabalho_Individual/Hash_Especial.py
@@ -12,9 +12,34 @@ class HashTable:
 
 def _hash(self, chave):
     chave = chave.lower()
-    return hash(chave) % self.M
+    soma = 0
+    for c in chave:
+        soma += ord(c)
+    return hash(soma) % self.M
 
+    def get(self, chave):
+        pos = self._hash(chave)
+        aux = self.tab[pos]
+        while aux:
+            if aux.key == chave:
+                return aux.value
+            aux = aux.prox
+        raise KeyError(chave)
 
+    def put(self, chave, valor):
+        pos = self._hash(chave)
+        
+        aux = self.tab[pos]
+        while aux:
+            if aux.key == chave:
+                aux.value = valor 
+                return
+            aux = aux.prox
+        
+        novo = Node(chave, valor)
+        novo.prox = self.tab[pos]
+        self.tab[pos] = novo
+        self.N += 1
 
 
 
